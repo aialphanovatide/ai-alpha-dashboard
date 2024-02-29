c4a41e178e14b609efcf1fc29febd9cf7bcc5304
agusbustadev <agustin.bustamante@chewedpixelstudios.com>
2024-02-29 17:18:51 -0300

    scheduled fix styles

--- src/views/analysis/ScheduledJob.js
+++ src/views/analysis/ScheduledJob.js
@@ -3,18 +3,29 @@ import CIcon from "@coreui/icons-react";
 import { cilTrash } from "@coreui/icons";
 
 const ScheduledJob = ({ job, onDelete }) => {
-  // Parse title from the HTML string
-  const title = job.args.match(/<p>(.*?)<\/p>/)[1];
+  // Dividir job.args en dos partes usando la coma como delimitador
+  const [, htmlString] = job.args.split(',');
 
-  // Extract date and time from job.trigger
+  // Eliminar espacios en blanco al principio y al final de la cadena HTML
+  const trimmedHtmlString = htmlString.trim();
+
+  // Crear un nuevo parser HTML
+  const parser = new DOMParser();
+  const parsedHtml = parser.parseFromString(trimmedHtmlString, "text/html");
+
+  // Obtener el título del elemento HTML
+  const titleElement = parsedHtml.querySelector("p");
+  const title = titleElement ? titleElement.textContent : "";
+
+  // Extraer fecha y hora de job.trigger
   const dateMatch = job.trigger.match(/\[(.*?)\]/);
   const nextRunDateTime = dateMatch
     ? dateMatch[1].replace(/ -\d{2}$/, "")
     : "Unknown";
 
   const handleDeleteClick = () => {
-    // Call the onDelete function passed as a prop
-    onDelete(job.id); // Assuming job has an id property
+    // Llamar a la función onDelete pasada como prop
+    onDelete(job.id); // Suponiendo que job tiene una propiedad id
   };
 
   return (
