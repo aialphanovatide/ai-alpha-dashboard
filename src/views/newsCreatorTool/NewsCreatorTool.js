176a0ba1e7d9c9513eb0e86d73878b7c42f233dd
Agustin Bustamante <agustinbustamante@Agustins-MBP-2.fibertel.com.ar>
2024-07-02 10:12:09 -0300

    Feat: pdf reader added to news creator

--- src/views/newsCreatorTool/NewsCreatorTool.js
+++ src/views/newsCreatorTool/NewsCreatorTool.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Form, Button, Spinner, Alert } from "react-bootstrap";
+import pdfToText from "react-pdftotext";
 import config from "../../config";
-import './NewsCreatorTook.css'
+import "./NewsCreatorTool.css";
 
 const NewsCreatorTool = () => {
   const [categories, setCategories] = useState([]);
@@ -12,21 +13,34 @@ const NewsCreatorTool = () => {
   const [analysis, setAnalysis] = useState("");
   const [usedKeywords, setUsedKeywords] = useState("");
   const [isArticleEfficient, setIsArticleEfficient] = useState("");
-  const [isLoadingRegenerateArticle, setIsLoadingRegenerateArticle] = useState(false);
-  const [isLoadingRegenerateImage, setIsLoadingRegenerateImage] = useState(false);
+  const [isLoadingRegenerateArticle, setIsLoadingRegenerateArticle] =
+    useState(false);
+  const [isLoadingRegenerateImage, setIsLoadingRegenerateImage] =
+    useState(false);
   const [isLoadingSave, setIsLoadingSave] = useState(false);
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
   const [previewData, setPreviewData] = useState(null);
   const [showPreview, setShowPreview] = useState(false);
   const [articleToBeSaved, setArticleToBeSaved] = useState(null);
   const [imageToBeSaved, setImageToBeSaved] = useState(null);
+  const [selectedExtractType, setSelectedExtractType] = useState("");
+  const [link, setLink] = useState("");
+  const [pdfFile, setPdfFile] = useState(null);
+  const [extractedContent, setExtractedContent] = useState("");
+  const [isExtracting, setIsExtracting] = useState(false);
 
   useEffect(() => {
     const fetchCategoriesAndBots = async () => {
       try {
-        const categoriesResponse = await fetch(`${config.BOTS_V2_API}/categories`);
+        const categoriesResponse = await fetch(
+          `${config.BOTS_V2_API}/categories`,
+        );
         const categoriesData = await categoriesResponse.json();
-        if (categoriesData && categoriesData.data && categoriesData.data.categories) {
+        if (
+          categoriesData &&
+          categoriesData.data &&
+          categoriesData.data.categories
+        ) {
           setCategories(categoriesData.data.categories);
         } else {
           console.error("Error fetching categories:", categoriesData.message);
@@ -51,20 +65,68 @@ const NewsCreatorTool = () => {
     fetchCategoriesAndBots();
   }, []);
 
+  const handleExtractContent = async () => {
+    setIsExtracting(true);
+    try {
+      if (selectedExtractType === "pdf" && pdfFile) {
+        const text = await pdfToText(pdfFile);
+        console.log(text);
+        setExtractedContent(text);
+        setAnalysis(text);
+      } else {
+        let requestData = {
+          extract_type: selectedExtractType,
+          link:
+            selectedExtractType === "link" ||
+            selectedExtractType === "google_docs"
+              ? link
+              : undefined,
+        };
+
+        console.log("requestData ", requestData);
+
+        const response = await fetch(`http://127.0.0.1:5001/extract_content`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "ngrok-skip-browser-warning": "true",
+          },
+          body: JSON.stringify(requestData),
+        });
+
+        const data = await response.json();
+        console.log("data ", data);
+        if (response.ok) {
+          setExtractedContent(data.response);
+          setAnalysis(data.response);
+        } else {
+          console.error("Error extracting content:", data.response);
+        }
+      }
+    } catch (error) {
+      console.error("Error extracting content:", error.message);
+    } finally {
+      setIsExtracting(false);
+    }
+  };
+
   const handleGenerate = async () => {
     setIsLoadingRegenerateArticle(true);
     try {
-      const articleResponse = await fetch(`${config.BOTS_V2_API}/generate_article`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "ngrok-skip-browser-warning": "true",
+      const articleResponse = await fetch(
+        `${config.BOTS_V2_API}/generate_article`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "ngrok-skip-browser-warning": "true",
+          },
+          body: JSON.stringify({
+            content: analysis,
+            category_id: selectedCategory,
+          }),
         },
-        body: JSON.stringify({
-          content: analysis,
-          category_id: selectedCategory,
-        }),
-      });
+      );
 
       const articleData = await articleResponse.json();
       if (!articleResponse.ok) {
@@ -72,17 +134,20 @@ const NewsCreatorTool = () => {
         return;
       }
 
-      const imageResponse = await fetch(`${config.BOTS_V2_API}/generate_image`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "ngrok-skip-browser-warning": "true",
+      const imageResponse = await fetch(
+        `${config.BOTS_V2_API}/generate_image`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "ngrok-skip-browser-warning": "true",
+          },
+          body: JSON.stringify({
+            content: articleData.data.content,
+            bot_id: selectedBot,
+          }),
         },
-        body: JSON.stringify({
-          content: articleData.data.content,
-          bot_id: selectedBot,
-        }),
-      });
+      );
 
       const imageData = await imageResponse.json();
       setIsLoadingRegenerateArticle(false);
@@ -104,7 +169,7 @@ const NewsCreatorTool = () => {
         content: articleData.data.content,
         analysis: analysis,
         used_keywords: usedKeywords,
-        is_article_efficent: isArticleEfficient,
+        is_article_efficient: isArticleEfficient,
         category_id: selectedCategory,
         bot_id: selectedBot,
       });
@@ -133,29 +198,45 @@ const NewsCreatorTool = () => {
           content: articleToBeSaved.content,
           analysis: articleToBeSaved.analysis,
           used_keywords: articleToBeSaved.used_keywords,
-          is_article_efficent: articleToBeSaved.is_article_efficent,
-          bot_id: articleToBeSaved.bot_id,
+          is_article_efficient: articleToBeSaved.is_article_efficient,
           category_id: articleToBeSaved.category_id,
-          image: imageToBeSaved,
+          bot_id: articleToBeSaved.bot_id,
+          image_url: imageToBeSaved,
         }),
       });
 
       const data = await response.json();
-      setIsLoadingSave(false);
-
       if (response.ok) {
-        setArticles([...articles, data.data]);
         setShowSuccessMessage(true);
-        setTimeout(() => setShowSuccessMessage(false), 3000);
+        setTimeout(() => setShowSuccessMessage(false), 10000);
+        setArticles((prevArticles) => [
+          ...prevArticles,
+          { ...articleToBeSaved, image_url: imageToBeSaved },
+        ]);
+        setShowPreview(false);
       } else {
-        console.error("Error saving article:", data.error);
+        console.error("Error saving article:", data.message);
       }
     } catch (error) {
       console.error("Error saving article:", error.message);
+    } finally {
       setIsLoadingSave(false);
-    }finally {
-        handleStartOver();
-      }
+    }
+  };
+
+  const handleStartOver = () => {
+    setSelectedCategory("");
+    setSelectedBot("");
+    setAnalysis("");
+    setUsedKeywords("");
+    setIsArticleEfficient("");
+    setSelectedExtractType("");
+    setLink("");
+    setPdfFile(null);
+    setExtractedContent("");
+    setShowPreview(false);
+    setArticleToBeSaved(null);
+    setImageToBeSaved(null);
   };
 
   const handleRegenerateArticle = async () => {
@@ -231,157 +312,252 @@ const NewsCreatorTool = () => {
     }
   };
 
-  const handleStartOver = () => {
-    setSelectedCategory("");
-    setSelectedBot("");
-    setAnalysis("");
-    setUsedKeywords("");
-    setIsArticleEfficient("");
-    setShowPreview(false);
-    setPreviewData(null);
-    setArticleToBeSaved(null);
-    setImageToBeSaved(null);
-  };
-
   return (
-    <div>
-      <div style={{ margin: "20px", overflowX: "auto" }}>
-        <h2>News Creator Tool</h2>
-        <br />
-        <Form.Group controlId="categorySelect" style={{ marginBottom: "15px" }}>
-          <Form.Label>Select Category</Form.Label>
+    <div className="news-creator-tool">
+      <h1>News Creator Tool</h1>
+      <Form>
+        <Form.Group controlId="extractType">
+          <Form.Label>Extract Content From</Form.Label>
+          <Form.Control
+            as="select"
+            value={selectedExtractType}
+            onChange={(e) => setSelectedExtractType(e.target.value)}
+          >
+            <option value="">Select Extract Type</option>
+            <option value="link">Link</option>
+            <option value="google_docs">Google Docs</option>
+            <option value="pdf">PDF</option>
+          </Form.Control>
+        </Form.Group>
+
+        {selectedExtractType === "link" ||
+        selectedExtractType === "google_docs" ? (
+          <Form.Group controlId="link">
+            <br></br>
+            <Form.Label>Link</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter link"
+              value={link}
+              onChange={(e) => setLink(e.target.value)}
+            />
+          </Form.Group>
+        ) : selectedExtractType === "pdf" ? (
+          <Form.Group controlId="pdfFile">
+            <br></br>
+            <Form.Label>PDF File</Form.Label>
+            <Form.Control
+              type="file"
+              accept="application/pdf"
+              onChange={(e) => setPdfFile(e.target.files[0])}
+            />
+          </Form.Group>
+        ) : null}
+        <br></br>
+        <Button
+          variant="primary"
+          onClick={handleExtractContent}
+          disabled={
+            isExtracting ||
+            !selectedExtractType ||
+            (selectedExtractType !== "pdf" && !link) ||
+            (selectedExtractType === "pdf" && !pdfFile)
+          }
+        >
+          {isExtracting ? (
+            <Spinner
+              as="span"
+              animation="border"
+              size="sm"
+              role="status"
+              aria-hidden="true"
+            />
+          ) : (
+            "Extract Content"
+          )}
+        </Button>
+        <hr></hr>
+        <Form.Group controlId="category">
+          <Form.Label>Category</Form.Label>
           <Form.Control
             as="select"
             value={selectedCategory}
             onChange={(e) => setSelectedCategory(e.target.value)}
           >
-            <option value="">Select...</option>
+            <option value="">Select Category</option>
             {categories.map((category) => (
               <option key={category.id} value={category.id}>
-                {category.name.toUpperCase()}
+                {category.name}
               </option>
             ))}
           </Form.Control>
         </Form.Group>
-        <Form.Group controlId="botSelect" style={{ marginBottom: "15px" }}>
-          <Form.Label>Select Bot</Form.Label>
+
+        <Form.Group controlId="bot">
+          <br></br>
+          <Form.Label>Bot</Form.Label>
           <Form.Control
             as="select"
             value={selectedBot}
             onChange={(e) => setSelectedBot(e.target.value)}
-            >
-              <option value="">Select...</option>
-              {bots.map((bot) => (
-                <option key={bot.id} value={bot.id}>
-                  {bot.name.toUpperCase()}
-                </option>
-              ))}
-            </Form.Control>
-          </Form.Group>
-          <Form.Group controlId="analysisInput" style={{ marginBottom: "15px"}}>
-            <Form.Label>Analysis</Form.Label>
-            <Form.Control
-              as="textarea"
-              rows={6} // Esto ajusta el número de filas visible del textarea
-              style={{ height: "200px", resize: "none" }} // Aquí se ajusta el tamaño del textarea
-              value={analysis}
-              onChange={(e) => setAnalysis(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group controlId="usedKeywordsInput" style={{ marginBottom: "15px" }}>
-            <Form.Label>Used Keywords</Form.Label>
-            <Form.Control
-              type="text"
-              value={usedKeywords}
-              onChange={(e) => setUsedKeywords(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group controlId="isArticleEfficientInput" style={{ marginBottom: "15px" }}>
-            <Form.Label>Is Article Efficient</Form.Label>
-            <Form.Control
-              type="text"
-              value={isArticleEfficient}
-              onChange={(e) => setIsArticleEfficient(e.target.value)}
-            />
-          </Form.Group>
-          <Button
-            onClick={handleGenerate}
-            style={{ marginRight: "5px" }}
-            disabled={isLoadingRegenerateArticle || isLoadingRegenerateImage || isLoadingSave}
           >
-            {isLoadingRegenerateArticle ? (
-              <>
-                <Spinner
-                  as="span"
-                  animation="border"
-                  size="sm"
-                  role="status"
-                  aria-hidden="true"
-                  style={{ marginRight: "5px" }}
-                />
-                Generating Article...
-              </>
-            ) : (
-              "Generate"
-            )}
-          </Button>
-          {showPreview && previewData && (
+            <option value="">Select Bot</option>
+            {bots.map((bot) => (
+              <option key={bot.id} value={bot.id}>
+                {bot.name}
+              </option>
+            ))}
+          </Form.Control>
+        </Form.Group>
+      </Form>
+
+      <Form.Group controlId="analysis">
+        <br></br>
+        <Form.Label>Analysis</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={3}
+          value={analysis}
+          onChange={(e) => setAnalysis(e.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group controlId="usedKeywords">
+        <br></br>
+        <Form.Label>Used Keywords</Form.Label>
+        <Form.Control
+          type="text"
+          value={usedKeywords}
+          onChange={(e) => setUsedKeywords(e.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group controlId="isArticleEfficient">
+        <br></br>
+        <Form.Label>Is Article Efficient?</Form.Label>
+        <Form.Control
+          type="text"
+          value={isArticleEfficient}
+          onChange={(e) => setIsArticleEfficient(e.target.value)}
+        />
+      </Form.Group>
+      <br></br>
+      <Button
+        variant="primary"
+        onClick={handleGenerate}
+        disabled={
+          isLoadingRegenerateArticle ||
+          !selectedCategory ||
+          !selectedBot ||
+          !analysis
+        }
+      >
+        {isLoadingRegenerateArticle ? (
+          <Spinner
+            as="span"
+            animation="border"
+            size="sm"
+            role="status"
+            aria-hidden="true"
+          />
+        ) : (
+          "Generate Article and Image"
+        )}
+      </Button>
+
+      <Button
+        variant="secondary"
+        onClick={handleStartOver}
+        style={{ marginLeft: "10px" }}
+        className="ml-2"
+      >
+        Start Over
+      </Button>
+
+      {showPreview && (
+        <div className="preview">
+          <br></br>
+          <h3>Preview:</h3>
+          <h4>{previewData.title}</h4>
+          <p>{previewData.content}</p>
+
           <Button
             variant="secondary"
-            onClick={handleStartOver}
-            className="start-over-button"
+            onClick={handleRegenerateArticle}
+            disabled={
+              isLoadingRegenerateArticle ||
+              !selectedCategory ||
+              !selectedBot ||
+              !analysis
+            }
           >
-            Start Over
+            {isLoadingRegenerateArticle ? (
+              <Spinner
+                as="span"
+                animation="border"
+                size="sm"
+                role="status"
+                aria-hidden="true"
+              />
+            ) : (
+              "Regenerate Article"
+            )}
           </Button>
-        )}
-          
-          <hr></hr>
-          {showPreview && previewData && (
-          <div className="preview-container">
-            <h3>Preview:</h3>
-            <p className="preview-content">Title: {previewData.title}</p>
-            <p className="preview-content">Content: {previewData.content}</p>
-            <Button
-              variant="secondary"
-              onClick={handleRegenerateArticle}
-              className="regenerate-article-button"
-              disabled={isLoadingRegenerateArticle}
-            >
-              {isLoadingRegenerateArticle ? "Regenerating..." : "Regenerate Article"}
-            </Button>
-            <br></br>
+          <br></br>
+          <br></br>
+
+          {previewData.image && (
             <img
               src={previewData.image}
-              alt="Article Poster"
-              className="preview-image"
-              style={{ width: "300px", height: "300px" }}
+              style={{ width: "200px", height: "200px" }}
+              alt="Generated"
             />
-            <br></br>
-            <Button
-              variant="secondary"
-              onClick={handleRegenerateImage}
-              className="regenerate-image-button"
-              disabled={isLoadingRegenerateImage}
-            >
-              {isLoadingRegenerateImage ? "Regenerating..." : "Regenerate Image"}
-            </Button>
-            <br></br>
-            <Button
-              variant="primary"
-              onClick={handleSave}
-              className="save-button"
-              disabled={isLoadingSave}
-            >
-              {isLoadingSave ? "Saving..." : "Save"}
-            </Button>
-            {showSuccessMessage && (
-            <Alert variant="success" style={{ marginTop: "10px" }}>
-              News created!
-            </Alert>
           )}
-          </div>
+          <br></br>
+          <br></br>
+
+          <Button
+            variant="secondary"
+            onClick={handleRegenerateImage}
+            disabled={
+              isLoadingRegenerateImage ||
+              !selectedBot ||
+              !previewData ||
+              !previewData.content
+            }
+          >
+            {isLoadingRegenerateImage ? (
+              <Spinner
+                as="span"
+                animation="border"
+                size="sm"
+                role="status"
+                aria-hidden="true"
+              />
+            ) : (
+              "Regenerate Image"
+            )}
+          </Button>
+        </div>
+      )}
+      <br></br>
+      <Button variant="success" onClick={handleSave} disabled={isLoadingSave}>
+        {isLoadingSave ? (
+          <Spinner
+            as="span"
+            animation="border"
+            size="sm"
+            role="status"
+            aria-hidden="true"
+          />
+        ) : (
+          "Save"
         )}
-      </div>
+      </Button>
+
+      {showSuccessMessage && (
+        <Alert variant="success">News created successfully!</Alert>
+      )}
     </div>
   );
 };
