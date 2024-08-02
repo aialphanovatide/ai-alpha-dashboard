0f2f138383c35b1bbb464aef1bba405e3be5e35c
agustin bustamante <agustinbustamante@Agustins-MBP-2.fibertel.com.ar>
2024-08-02 16:32:38 -0300

    top story section working complete

--- src/views/topStories/topStories.js
+++ src/views/topStories/topStories.js
@@ -1,123 +1,111 @@
 import React, { useEffect, useState } from 'react';
 import config from "src/config";
-import '../analysis/analysis.css'
-import Swal from 'sweetalert2'
-import CIcon from '@coreui/icons-react'
-import { cilTrash } from '@coreui/icons'
-import './topStories.css'
-
+import '../analysis/analysis.css';
+import Swal from 'sweetalert2';
+import CIcon from '@coreui/icons-react';
+import { cilTrash } from '@coreui/icons';
+import './topStories.css';
+import { format } from 'date-fns';
 
 const Modal = ({ item, base64Image, onClose }) => {
-
-  const formattedSummary = item.summary.replace(/\n/g, '<br>')
+    const formattedSummary = item.analysis ? item.analysis.replace(/\n/g, '<br>') : item.content.replace(/\n/g, '<br>')
 
     return (
-      <div className="modalOverlay" onClick={onClose}>
-        <div className="modalContent" onClick={(e) => e.stopPropagation()}>
-          {base64Image && (
-            <img
-              className="modalImage"
-              src={`data:image/png;base64,${base64Image}`}
-              alt="top sotry"
-            />
-          )}
-        <span className="modalText" dangerouslySetInnerHTML={{ __html: formattedSummary }} />
-          <button className="closeButton" onClick={onClose}>
-            Close
-          </button>
+        <div className="modalOverlay" onClick={onClose}>
+            <div className="modalContent" onClick={(e) => e.stopPropagation()}>
+                {base64Image && (
+                    <img
+                        className="modalImage"
+                        src={`https://appnewsposters.s3.us-east-2.amazonaws.com/${base64Image}`}
+                        alt="top story"
+                    />
+                )}
+                <span className="modalText" dangerouslySetInnerHTML={{ __html: formattedSummary }} />
+                <button className="closeButton" onClick={onClose}>
+                    Close
+                </button>
+            </div>
         </div>
-      </div>
     );
-  };
-
+};
 
 const Item = ({ item, base64Image, onDelete }) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     const handleClick = (e) => {
-        onDelete(item.top_story_id);
-      };
-  
-      const handleItemClick = (e) => {
+        e.stopPropagation();
+        onDelete(item.id);  // Usamos article_id aquí
+    };
 
-        // Check if the click target or its parent has the deleteBtn class
-        if (
-            e.target.classList.contains('deleteBtn') ||
-            e.target.parentElement.classList.contains('deleteBtn')
-        ) {
-            // If it is, do not open the modal
+    const handleItemClick = (e) => {
+        if (e.target.classList.contains('deleteBtn') || e.target.parentElement.classList.contains('deleteBtn')) {
             return;
         }
         setIsModalOpen(true);
-      };
-  
-    const handleCloseModal = () => {
-      setIsModalOpen(false);
     };
 
-    const formattedSummary = item.summary.replace(/\n/g, '<br>')
-  
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
+
+    const formattedSummary = item.analysis ? item.analysis.replace(/\n/g, '<br>') : item.content.replace(/\n/g, '<br>')
+    const formattedDate = format(new Date(item.date), 'MMMM dd, yyyy');
+    console.log(item)
     return (
-      <>
-        <li className="allTopStoriesLI" onClick={handleItemClick}>
-          {base64Image && (
-            <img
-              className="itemImage"
-              src={`data:image/png;base64,${base64Image}`}
-              alt="Analysis"
-            />
-          )}
-          <span className="itemContent" dangerouslySetInnerHTML={{ __html: formattedSummary }} />
-          {onDelete && <CIcon size="xxl" icon={cilTrash} className="deleteBtn" onClick={handleClick} />}
-        </li>
-        {isModalOpen && (
-          <Modal item={item} base64Image={base64Image} onClose={handleCloseModal} />
-        )}
-      </>
+        <>
+            <div className="card" onClick={handleItemClick}>
+                {base64Image && (
+                    <img
+                        className="card-img-top"
+                        src={`https://appnewsposters.s3.us-east-2.amazonaws.com/${base64Image}`}
+                        alt="Analysis"
+                    />
+                )}
+                <div className="card-body">
+                    <h5 className="card-title">{item.title}</h5>
+                    <p className="card-text" dangerouslySetInnerHTML={{ __html: formattedSummary }} />
+                    <p className="card-date">{formattedDate}{onDelete && <CIcon size="xxl" icon={cilTrash} className="deleteBtn" onClick={handleClick} />}</p>
+                </div>
+            </div>
+            {isModalOpen && (
+                <Modal item={item} base64Image={base64Image} onClose={handleCloseModal} />
+            )}
+        </>
     );
-  };
-  
+};
 
 const TopStories = () => {
+    const [topStories, setTopStories] = useState([]);
 
-    const [topStories, setTopStories] = useState([])
-    
-    // gets all the top stories
     const getTopStories = async () => {
         try {
-            const response = await fetch(`${config.BASE_URL}/api/get/allTopStories`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'ngrok-skip-browser-warning': 'true',
-                },
-            });
-    
-            const data = await response.json();
-    
-            if (response.ok) {
-                setTopStories(data.top_stories)
+            const response1 = await fetch(`${config.BOTS_V2_API}/get_all_articles?limit=200`);
+            const data1 = await response1.json();
+
+            if (response1.ok) {
+                const topStories = data1.data.filter(article => article.is_top_story);
+                console.log(topStories)
+                setTopStories(topStories);
             } else {
-                console.error('Error fetching:', data.top_stories);
+                console.error('Error fetching:', data1);
             }
         } catch (error) {
             console.error('Error fetching:', error);
         }
     };
 
-    // Deletes a top story
-    const handleDelete = async (top_story_id) => {
+    const handleDelete = async (article_id) => {
         try {
-            const response = await fetch(`${config.BASE_URL}/api/delete/top-story/${top_story_id}`, {
-                method: 'DELETE',
+            const response = await fetch(`${config.BOTS_V2_API}/api/update/top-story/${article_id}`, {
+                method: 'PUT',  
                 headers: {
                     'Content-Type': 'application/json',
                     'ngrok-skip-browser-warning': 'true',
                 },
             });
-    
+
             const data = await response.json();
-    
+
             if (response.ok) {
                 Swal.fire({
                     icon: "success",
@@ -125,18 +113,17 @@ const TopStories = () => {
                     showConfirmButton: false,
                     timer: 1500
                 });
-                getTopStories() 
+                getTopStories();
             } else {
-                console.error('Error deleting:', data);
+                console.error('Error updating:', data);
                 Swal.fire({
                     icon: "error",
                     title: data.error,
                     showConfirmButton: false,
-                    
                 });
             }
         } catch (error) {
-            console.error('Error deleting:', error);
+            console.error('Error updating:', error);
             Swal.fire({
                 icon: "error",
                 title: error,
@@ -145,22 +132,23 @@ const TopStories = () => {
         }
     };
 
-    useEffect(()=> {
-        getTopStories()
-    }, [])
-    
-  return (
-    <div className='alltopStoriesmain'>
-      <h3 className='alltopStoriesTitle'>Top Stories</h3>
-      {topStories && topStories.length > 0 ?
-      <ul className='alltopStoriesUL'>
-      {topStories?.map(item => (
-        <Item key={item.top_story_id} item={item} onDelete={handleDelete}  />
-      ))}
-        </ul>: <span>No Top Stories yet</span>
-      }
-    </div>
-  );
+    useEffect(() => {
+        getTopStories();
+    }, []);
+
+    return (
+        <div className='alltopStoriesmain'>
+            <h3 className='alltopStoriesTitle'>Top Stories</h3>
+            <br />
+            {topStories && topStories.length > 0 ?
+                <div className='card-deck'>
+                    {topStories.map(item => (
+                        <Item key={item.article_id} item={item} base64Image={item.image} onDelete={handleDelete} />
+                    ))}
+                </div> : <span>No Top Stories yet</span>
+            }
+        </div>
+    );
 };
 
-export default TopStories
+export default TopStories;
