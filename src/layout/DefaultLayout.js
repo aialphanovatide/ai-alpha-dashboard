715e5e73516863cfc450d89a52bdf4f0dac24a68
Camila Lucía Sosa <camilaluciasosa@hotmail.com>
2024-08-15 12:59:05 -0300

    fix: styles: remove margin bottom from header and add it to app content parent container padding

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -7,7 +7,7 @@ const DefaultLayout = () => {
       <AppSidebar />
       <div className="wrapper d-flex flex-column min-vh-100">
         <AppHeader />
-        <div className="body flex-grow-1">
+        <div className="body flex-grow-1 p-4">
           <AppContent />
         </div>
         <AppFooter />
