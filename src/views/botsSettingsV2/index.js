c25c3697fd9001a117b3f92a14e551be5b5a6885
Camila Lucía Sosa <camilaluciasosa@hotmail.com>
2024-08-26 16:26:31 -0300

    refactor: modularize bot list methods

--- src/views/botsSettingsV2/index.js
+++ src/views/botsSettingsV2/index.js
@@ -5,7 +5,7 @@ import BotList from "./components/BotList";
 import AddWordsModal from "../addWordsModal/AddWordsModal";
 import DeleteWordsModal from "../deleteWordsModal/DeleteWordsModal";
 import DeleteBlacklistWordsModal from "../deleteBlacklistWordsModal/DeleteBlacklistWordsModal";
-import config from "../../config";
+
 import UsedKeywordsModal from "../usedKeywordsModal/UsedKeywordsModal";
 import CreateBotModal from "../createBotModal/CreateBotModal";
 import CreateCategoryModal from "../createCategoryModal/CreateCategoryModal";
@@ -39,19 +39,147 @@ const BotsSettings = () => {
 
   const getAllBots = useCallback(async () => {
     try {
-      const response = await fetch(`${config.BOTS_V2_API}/get_all_bots`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          "ngrok-skip-browser-warning": "true",
-        },
-      });
+      // const response = await fetch(`${config.BOTS_V2_API}/get_all_bots`, {
+      //   method: "GET",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //     "ngrok-skip-browser-warning": "true",
+      //   },
+      // });
 
-      const responseText = await response.text();
-      console.log("responseText : ", responseText);
+      // const responseText = await response.text();
+      // console.log("responseText : ", responseText);
 
       try {
-        const data = JSON.parse(responseText);
+        let bots2 = {
+          data: [
+            {
+              alias: "Bitcoin",
+              category: "bitcoin",
+              color: "#FC5404",
+              icon: "/static/topmenu_icons_resize/bitcoin.png",
+              isActive: true,
+              updated_at: "Mon, 26 Aug 2024 11:05:53 GMT",
+            },
+            {
+              alias: "Ethereum",
+              category: "ethereum",
+              color: "#325C86",
+              icon: "/static/topmenu_icons_resize/ethereum.png",
+              isActive: true,
+              updated_at: "Mon, 26 Aug 2024 11:55:53 GMT",
+            },
+            {
+              alias: "Hacks",
+              category: "hacks",
+              color: "#325C86",
+              icon: "/static/topmenu_icons_resize/baseblock.png",
+              isActive: true,
+              updated_at: "Mon, 26 Aug 2024 11:09:54 GMT",
+            },
+            {
+              alias: "Lsd",
+              category: "lsd",
+              color: "#FFC53C",
+              icon: "/static/topmenu_icons_resize/lsds.png",
+              isActive: true,
+              updated_at: "Mon, 26 Aug 2024 11:56:53 GMT",
+            },
+            {
+              alias: "RootLink",
+              category: "layer 0",
+              color: "#802291",
+              icon: "/static/topmenu_icons_resize/rootlink.png",
+              isActive: true,
+              updated_at: "Mon, 26 Aug 2024 11:35:32 GMT",
+            },
+            {
+              alias: "BaseBlock",
+              category: "layer 1 lmc",
+              color: "#0B84CE",
+              icon: "/static/topmenu_icons_resize/baseblock.png",
+              isActive: true,
+              updated_at: "Mon, 26 Aug 2024 11:57:53 GMT",
+            },
+            {
+              alias: "CoreChain",
+              category: "layer 1 mmc",
+              color: "#FDE74B",
+              icon: "/static/topmenu_icons_resize/corechain.png",
+              isActive: true,
+              updated_at: "Mon, 26 Aug 2024 11:16:53 GMT",
+            },
+            {
+              alias: "BoostLayer",
+              category: "layer 2",
+              color: "#5BD83D",
+              icon: "/static/topmenu_icons_resize/boostlayer.png",
+              isActive: true,
+              updated_at: "Mon, 26 Aug 2024 11:58:53 GMT",
+            },
+            {
+              alias: "TruthNodes",
+              category: "oracle",
+              color: "#389AEA",
+              icon: "/static/topmenu_icons_resize/truthnodes.png",
+              isActive: true,
+              updated_at: "Mon, 26 Aug 2024 11:29:54 GMT",
+            },
+            {
+              alias: "X Payments",
+              category: "cross border payments",
+              color: "#51DD8C",
+              icon: "/static/topmenu_icons_resize/x_payments.png",
+              isActive: true,
+              updated_at: "Mon, 26 Aug 2024 11:49:53 GMT",
+            },
+            {
+              alias: "CycleSwap",
+              category: "defip",
+              color: "#20CBDD",
+              icon: "/static/topmenu_icons_resize/cycle_swap.png",
+              isActive: true,
+              updated_at: "Mon, 26 Aug 2024 11:43:53 GMT",
+            },
+            {
+              alias: "NexTrade",
+              category: "defi",
+              color: "#FF39C2",
+              icon: "/static/topmenu_icons_resize/nextrade.png",
+              isActive: true,
+              updated_at: "Mon, 26 Aug 2024 11:36:32 GMT",
+            },
+            {
+              alias: "DiverseFi",
+              category: "defio",
+              color: "#C438B3",
+              icon: "/static/topmenu_icons_resize/diverse_fi.png",
+              isActive: true,
+              updated_at: "Mon, 26 Aug 2024 11:35:54 GMT",
+            },
+            {
+              alias: "IntelliChain",
+              category: "ai",
+              color: "#895DF6",
+              icon: "/static/topmenu_icons_resize/intellichain.png",
+              isActive: true,
+              updated_at: "Mon, 26 Aug 2024 11:25:53 GMT",
+            },
+            {
+              alias: "Metals",
+              category: "metals",
+              color: "#895DF6",
+              icon: "/static/topmenu_icons_resize/metals.png",
+              isActive: true,
+              updated_at: "Mon, 26 Aug 2024 11:36:53 GMT",
+            },
+          ],
+          error: null,
+          success: true,
+        };
+
+        const data = bots2;
+        // const data = JSON.parse(responseText);
         console.log("data : ", data);
         if (data && data.data) {
           setBots(data.data);
@@ -67,48 +195,6 @@ const BotsSettings = () => {
       setLoading(false);
     }
   }, []);
-
-  const updateBotState = useCallback(
-    async (url, botCategory) => {
-      try {
-        const response = await fetch(url, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ category: botCategory }),
-        });
-
-        const data = await response.json();
-        if (data) {
-          await getAllBots();
-          console.log(`Bot ${botCategory} Updated After:`, data.bot);
-        } else {
-          console.error(
-            `Error At ${
-              url === "activate_bot" ? "TurnON" : "TurnOFF"
-            } the bot ${botCategory}:`,
-            data.message,
-          );
-        }
-      } catch (error) {
-        console.error("Error during:", error);
-      } finally {
-        setLoading(false);
-      }
-    },
-    [getAllBots],
-  );
-
-  const turnOnAllBots = useCallback(() => {
-    setLoading(true);
-    updateBotState(`${config.BOTS_V2_API}/activate_all_categories`);
-  }, [updateBotState]);
-
-  const turnOffAllBots = useCallback(() => {
-    setLoading(true);
-    updateBotState(`${config.BOTS_V2_API}/deactivate_all_categories`);
-  }, [updateBotState]);
 
   useEffect(() => {
     getAllBots();
@@ -136,7 +222,7 @@ const BotsSettings = () => {
             </button>
           </div>
         </div>
-        <div className="create" style={{ width: "40%" }}>
+        <div className="create" style={{ width: "30%" }}>
           <span>Create</span>
           <div
             style={{
@@ -153,9 +239,6 @@ const BotsSettings = () => {
             <button>
               <CIcon icon={cilPlus} /> New Bot
             </button>
-            <button>
-              <CIcon icon={cilPlus} /> New Coin
-            </button>
           </div>
         </div>
         <div className="keywords" style={{ width: "40%" }}>
@@ -163,8 +246,7 @@ const BotsSettings = () => {
           <div>
             <div>
               <span>
-                <CIcon icon={cilLockUnlocked} />{" "}
-                Whitelist
+                <CIcon icon={cilLockUnlocked} /> Whitelist
               </span>
               <div>
                 <button>
@@ -177,8 +259,7 @@ const BotsSettings = () => {
             </div>
             <div>
               <span>
-                <CIcon icon={cilLockLocked} />{" "}
-                Blacklist
+                <CIcon icon={cilLockLocked} /> Blacklist
               </span>
               <div>
                 <button>
@@ -193,10 +274,27 @@ const BotsSettings = () => {
           <div></div>
         </div>
       </div>
-      <div style={{ height: "70%" }}>{/* <BotList bots={bots} /> */}</div>
+      <div style={{ height: "70%" }}>
+        <BotList bots={bots} getAllBots={getAllBots} />
+      </div>
       <DrawerComponent toggleDrawer={toggleDrawer} open={open}>
         <span>hola</span>
       </DrawerComponent>
+      
+      {/* <div className="actionmain">
+        <h4 className="actionsTitle">Actions</h4>
+        <div className="actionsSubMain">
+          <CreateCategoryModal />
+          <DeleteCategoryModal />
+          <CreateBotModal />
+          <AddWordsModal />
+          <DeleteWordsModal />
+          <AddBlacklistWordsModal />
+          <DeleteBlacklistWordsModal />
+          <UsedKeywordsModal />
+        </div>
+      </div>  */}
+
     </div>
 
     // <div>
