b6f20a9d3b466b6eb65c9a0816c400d4b02a78d0
Camila Lucía Sosa <camilaluciasosa@hotmail.com>
2024-08-26 16:24:47 -0300

    feat: implement checkbox bot list

--- src/views/botsSettingsV2/components/BotList/index.js
+++ src/views/botsSettingsV2/components/BotList/index.js
@@ -1,120 +1,18 @@
-// import React, { useState, useEffect } from "react";
-// import "../../index.css";
-// import Loader from "../../../loader/loader";
-// import baseURL from "../../../../config";
-// import CIcon from "@coreui/icons-react";
-// import { cilToggleOn, cilToggleOff } from "@coreui/icons";
+import React, { useState, useEffect, useCallback } from "react";
+import "./index.css";
+import baseURL from "../../../../config";
+import CIcon from "@coreui/icons-react";
+import {
+  cilToggleOn,
+  cilToggleOff,
+  cilTrash,
+  cilPen,
+  // cilTerminal,
+} from "@coreui/icons";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-// const SpinnerComponent = () => {
-//   return (
-//     <div className="spinnergnral">
-//       <span
-//         className="spinner-border spinner-border-lg"
-//         aria-hidden="true"
-//       ></span>
-//     </div>
-//   );
-// };
-
-// // Función para formatear la fecha
-// const formatDate = (dateString) => {
-//   const date = new Date(dateString);
-//   const options = {
-//     day: "2-digit",
-//     month: "short",
-//     year: "numeric",
-//     hour: "2-digit",
-//     minute: "2-digit",
-//     hour12: false,
-//   };
-//   // Formatear la fecha y hora según las opciones
-//   return date.toLocaleString("en-GB", options).replace(/,/, "");
-// };
-
-// const BotList = ({ bots }) => {
-//   const [botList, setBotList] = useState([]);
-
-//   useEffect(() => {
-//     setBotList(bots); // Actualiza el estado 'botList' con el valor de 'bots'
-//   }, [bots]); // Establece 'bots' como la dependencia del efecto
-
-//   // Función para activar o desactivar un bot
-//   const toggleBotState = async (index) => {
-//     // Asegurarse de que botList no sea undefined
-//     if (botList && botList[index]) {
-//       const bot = botList[index];
-//       const url = bot.isActive
-//         ? `${baseURL.BOTS_V2_API}/deactivate_bot_by_id/${bot.category}`
-//         : `${baseURL.BOTS_V2_API}/activate_bot_by_id/${bot.category}`;
-
-//       try {
-//         const response = await fetch(url, { method: "POST" });
-//         if (response.ok) {
-//           const data = await response.json();
-
-//           const updatedBots = [...botList];
-//           updatedBots[index].isActive = !bot.isActive;
-//           setBotList(updatedBots);
-//         } else {
-//           console.error("Error:", response.statusText);
-//         }
-//       } catch (error) {
-//         console.error("Error:", error);
-//       }
-//     } else {
-//       console.error("Bot or botList is undefined.");
-//     }
-//   };
-
-//   return (
-//     <div className="bot-list-container">
-//       {botList && botList.length > 0 ? (
-//         botList.map((bot, index) => (
-//           <div
-//             key={index}
-//             className={`bot-item ${bot.isActive ? "activeBot" : "inactiveBot"}`}
-//           >
-//             <div className="bot-icon">
-//               <img
-//                 src={`https://aialphaicons.s3.us-east-2.amazonaws.com/${bot.alias.toLowerCase()}.png`}
-//                 alt={bot.alias}
-//                 style={{ width: 60, height: 60 }}
-//               />
-//             </div>
-//             <div className="bot-details">
-//               <div className="bot-category">{bot.category}</div>
-//               <div className="bot-alias">{bot.alias}</div>
-//               <div className="bot-alias">
-//                 Last Run: {formatDate(bot.updated_at)}
-//               </div>
-//             </div>
-//             <button
-//               className={`actdeactBtn ${
-//                 bot.isActive ? "activeBot" : "inactiveBot"
-//               }`}
-//               onClick={() => toggleBotState(index)}
-//             >
-//               {bot.isActive ? (
-//                 <CIcon icon={cilToggleOn} className="text-secondary"/>
-//               ) : (
-//                 <CIcon icon={cilToggleOff} className="text-secondary"/>
-//               )}
-//             </button>
-//           </div>
-//         ))
-//       ) : (
-//         // <Loader />
-//         <SpinnerComponent />
-//       )}
-//     </div>
-//   );
-// };
-
-// export default BotList;
-
-import React, { useState } from "react";
-
-const ListItem = ({ item }) => {
+const ListItem = ({ bot }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [isChecked, setIsChecked] = useState(false);
 
@@ -122,62 +20,213 @@ const ListItem = ({ item }) => {
   const toggleChecked = () => setIsChecked(!isChecked);
 
   return (
-    <div style={{ marginLeft: item.level * 20 }}>
-      <div>
-        <input type="checkbox" checked={isChecked} onChange={toggleChecked} />
-        <span onClick={toggleOpen} style={{ cursor: "pointer" }}>
-          {item.name}{" "}
-          {item.subItems && item.subItems.length > 0 && (isOpen ? "▲" : "▼")}
-        </span>
+    <>
+      <div className={`bot-item ${isChecked ? "checked" : ""}`}>
+        <div className="bot-item-input">
+          <input type="checkbox" checked={isChecked} onChange={toggleChecked} />
+        </div>
+        <div className="bot-img-container">
+          <img
+            src={`https://aialphaicons.s3.us-east-2.amazonaws.com/${bot.alias.toLowerCase()}.png`}
+            alt={bot.alias}
+          />
+        </div>
+        <div className="middle-container">
+          <div className="bot-details">
+            <div className="bot-category">{bot.category}</div>
+            <div>{bot.alias}</div>
+            <div className="bot-last-run">
+              Last Run: {formatDate(bot.updated_at)}
+            </div>
+          </div>
+          <button className="edit-btn">
+            <CIcon icon={cilPen} />
+            {"  "}
+            Edit
+          </button>
+          {/* <button className="prompt-btn">
+            <CIcon icon={cilTerminal} />{"  "}
+            Prompt
+          </button> */}
+          <button
+            className={`switch-btn ${
+              bot.isActive ? "activeBot" : "inactiveBot"
+            }`}
+            // onClick={() => toggleBotState(index)}
+          >
+            {bot.isActive ? (
+              <CIcon icon={cilToggleOn} size="xxl" />
+            ) : (
+              <CIcon icon={cilToggleOff} size="xxl" />
+            )}
+          </button>
+        </div>
+        <button onClick={toggleOpen} className="chevron-button">
+          {/* {bot.subItems && bot.subItems.length > 0 && (isOpen ? <ExpandLessIcon /> : <ExpandMoreIcon />)} */}
+          {isOpen ? (
+            <ExpandLessIcon color="inherit" fontSize="inherit" />
+          ) : (
+            <ExpandMoreIcon color="inherit" fontSize="inherit" />
+          )}
+        </button>
       </div>
-      {isOpen && item.subItems && (
+      {isOpen && bot.subItems && (
         <div>
-          {item.subItems.map((subItem) => (
-            <ListItem key={subItem.id} item={subItem} />
+          {bot.subItems.map((subItem, index) => (
+            <ListItem key={index} item={subItem} />
           ))}
         </div>
       )}
-    </div>
+    </>
   );
 };
 
-const List = ({ items }) => {
+const SpinnerComponent = () => {
   return (
-    <div>
-      {items.map((item) => (
-        <ListItem key={item.id} item={item} />
-      ))}
+    <div className="spinnergnral">
+      <span
+        className="spinner-border spinner-border-lg"
+        aria-hidden="true"
+      ></span>
     </div>
   );
 };
 
-const BotList = () => {
-  const items = [
-    {
-      id: 1,
-      name: "Item 1",
-      level: 0,
-      subItems: [
-        { id: 2, name: "SubItem 1.1", level: 1 },
-        { id: 3, name: "SubItem 1.2", level: 1 },
-      ],
-    },
-    {
-      id: 4,
-      name: "Item 2",
-      level: 0,
-      subItems: [
-        { id: 5, name: "SubItem 2.1", level: 1 },
-        {
-          id: 6,
-          name: "SubItem 2.2",
-          level: 1,
-        },
-      ],
-    },
-  ];
+// Función para formatear la fecha
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const options = {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  };
+  // Formatear la fecha y hora según las opciones
+  return date.toLocaleString("en-GB", options).replace(/,/, "");
+};
 
-  return <List items={items} />;
+const BotList = ({ bots, getAllBots }) => {
+  const [botList, setBotList] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const updateBotState = useCallback(
+    async (url, botCategory) => {
+      try {
+        const response = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ category: botCategory }),
+        });
+
+        const data = await response.json();
+        if (data) {
+          await getAllBots();
+          console.log(`Bot ${botCategory} Updated After:`, data.bot);
+        } else {
+          console.error(
+            `Error At ${
+              url === "activate_bot" ? "TurnON" : "TurnOFF"
+            } the bot ${botCategory}:`,
+            data.message,
+          );
+        }
+      } catch (error) {
+        console.error("Error during:", error);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [getAllBots],
+  );
+
+  const turnOnAllBots = useCallback(() => {
+    setLoading(true);
+    updateBotState(`${baseURL.BOTS_V2_API}/activate_all_categories`);
+  }, [updateBotState]);
+
+  const turnOffAllBots = useCallback(() => {
+    setLoading(true);
+    updateBotState(`${baseURL.BOTS_V2_API}/deactivate_all_categories`);
+  }, [updateBotState]);
+
+  useEffect(() => {
+    setBotList(bots); // Actualiza el estado 'botList' con el valor de 'bots'
+  }, [bots]); // Establece 'bots' como la dependencia del efecto
+
+  // Función para activar o desactivar un bot
+  const toggleBotState = async (index) => {
+    // Asegurarse de que botList no sea undefined
+    if (botList && botList[index]) {
+      const bot = botList[index];
+      const url = bot.isActive
+        ? `${baseURL.BOTS_V2_API}/deactivate_bot_by_id/${bot.category}`
+        : `${baseURL.BOTS_V2_API}/activate_bot_by_id/${bot.category}`;
+
+      try {
+        const response = await fetch(url, { method: "POST" });
+        if (response.ok) {
+          const data = await response.json();
+
+          const updatedBots = [...botList];
+          updatedBots[index].isActive = !bot.isActive;
+          setBotList(updatedBots);
+        } else {
+          console.error("Error:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    } else {
+      console.error("Bot or botList is undefined.");
+    }
+  };
+
+  return (
+    <>
+      <div className="top-container">
+        <button className="trash-btn">
+          <CIcon icon={cilTrash} size="xl" />
+        </button>
+        <button
+          className="pause-btn"
+          onClick={
+            loading
+              ? null
+              : botList.every((bot) => bot.isActive)
+                ? turnOffAllBots
+                : turnOnAllBots
+          }
+        >
+          <span>
+            {botList.every((bot) => bot.isActive)
+              ? "Pause All Bots"
+              : "Activate All Bots"}
+          </span>
+        </button>
+      </div>
+      <div className="bot-list-container">
+        {loading ? (
+          <div
+            style={{
+              height: "100%",
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <SpinnerComponent />
+          </div>
+        ) : (
+          botList.map((bot, index) => <ListItem key={index} bot={bot} />)
+        )}
+      </div>
+    </>
+  );
 };
 
 export default BotList;
