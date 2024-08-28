f7974572280197a50ad02f8f6c649a878fde97e4
Camila Lucía Sosa <camilaluciasosa@hotmail.com>
2024-08-28 11:44:10 -0300

    feat: add message param

--- src/components/NoData/index.js
+++ src/components/NoData/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import styles from "./index.module.css";
 import warnImg from "../../assets/images/exclamation.png";
 
-const NoData = ({ width = "100%", height = "100%" }) => {
+const NoData = ({ width = "100%", height = "100%", message = "No data found!" }) => {
   return (
     <div className={styles.container} style={{ width, height }}>
       <img src={warnImg} className="waring-sign-png" />
-      <h2>No data found!</h2>
+      <h2>{message}</h2>
     </div>
   );
 };
