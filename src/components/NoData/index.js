ffa79b1303a99b0b8399988edf5ed22c33a895bd
Camila Lucía Sosa <camilaluciasosa@hotmail.com>
2024-08-28 11:21:14 -0300

    feat: add default width and height values

--- src/components/NoData/index.js
+++ src/components/NoData/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import "./index.css";
+import styles from "./index.module.css";
 import warnImg from "../../assets/images/exclamation.png";
 
-const NoData = ({ width, height }) => {
+const NoData = ({ width = "100%", height = "100%" }) => {
   return (
-    <div className="container" style={{ width, height }}>
+    <div className={styles.container} style={{ width, height }}>
       <img src={warnImg} className="waring-sign-png" />
       <h2>No data found!</h2>
     </div>
