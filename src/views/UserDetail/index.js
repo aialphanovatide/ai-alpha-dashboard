080ac902752d8410d78a3b5ff40a15707e9032dd
Camila Lucía Sosa <camilaluciasosa@hotmail.com>
2024-09-20 15:14:11 -0300

    Add title and fix styles

--- src/views/UserDetail/index.js
+++ src/views/UserDetail/index.js
@@ -6,6 +6,7 @@ import "./index.css";
 import CIcon from "@coreui/icons-react";
 import { cilCheckCircle, cilClock } from "@coreui/icons";
 import SpinnerComponent from "src/components/Spinner";
+import Title from "src/components/commons/Title";
 
 const UserDetail = () => {
   const { user_id } = useParams();
@@ -41,56 +42,59 @@ const UserDetail = () => {
     getUser();
   }, []);
 
-  return isLoading ? (
-    <SpinnerComponent />
-  ) : (
+  return (
     <>
-      <div className="rounded overflow-hidden shadow-lg user-info-container">
-        <img className="user-img" src={user.picture || userImg} />
-        <CIcon
-          icon={cilCheckCircle}
-          className="text-info"
-          size="xxl"
-          style={{ visibility: user.email_verified ? "visible" : "hidden" }}
-        />
-        <div className="user-info-subcontainer bg-red-100">
-          <p className="name">{user.nickname}</p>
-          <p className="email">{user.email}</p>
-          <p className="id text-gray-600 dark:text-gray-300">
-            {user.auth0id}
-          </p>
-        </div>
-      </div>
-      <div className="mt-4 rounded overflow-hidden shadow-lg user-plans-container">
-        <h3 className="text-lg font-bold text-gray-900 dark:text-white plans-title">
-          Purchased Plans
-        </h3>
-        {user.purchased_plans?.length > 0 ? (
-          user.purchased_plans.map((plan, index) => (
-            <div
-              key={index}
-              className="my-2 rounded border border-gray-600 p-3 rounded-lg"
-            >
-              <div className="plan-info">
-                <p className="plan-name">{plan.reference_name}</p>
-                <p className="">
-                  <strong>Subscribed:</strong>{" "}
-                  {plan.is_subscribed ? "Yes" : "No"}
-                </p>
-                <p className="">
-                  <CIcon icon={cilClock} className="clock-icon"/>{" "}
-                  {new Date(plan.created_at).toLocaleDateString()}
-                </p>
-              </div>
-              <p className="price">${plan.price}</p>
+      <Title>User Detail</Title>
+      {isLoading ? (
+        <SpinnerComponent />
+      ) : (
+        <>
+          <div className="rounded overflow-hidden shadow user-info-container">
+            <img className="user-img" src={user.picture || userImg} />
+            <CIcon
+              icon={cilCheckCircle}
+              className="text-info"
+              size="xxl"
+              style={{ visibility: user.email_verified ? "visible" : "hidden" }}
+            />
+            <div className="user-info-subcontainer bg-red-100">
+              <p className="name">{user.nickname}</p>
+              <p className="email">{user.email}</p>
+              <p className="id text-gray-600 dark:text-gray-300">
+                {user.auth0id}
+              </p>
             </div>
-          ))
-        ) : (
-          <p className="font-bold">
-            No plans purchased
-          </p>
-        )}
-      </div>
+          </div>
+          <div className="mt-4 rounded overflow-hidden shadow user-plans-container">
+            <h3 className="text-lg font-bold text-gray-900 dark:text-white plans-title">
+              Purchased Plans
+            </h3>
+            {user.purchased_plans?.length > 0 ? (
+              user.purchased_plans.map((plan, index) => (
+                <div
+                  key={index}
+                  className="my-2 rounded border border-gray-600 p-3 rounded-lg"
+                >
+                  <div className="plan-info">
+                    <p className="plan-name">{plan.reference_name}</p>
+                    <p className="">
+                      <strong>Subscribed:</strong>{" "}
+                      {plan.is_subscribed ? "Yes" : "No"}
+                    </p>
+                    <p className="">
+                      <CIcon icon={cilClock} className="clock-icon" />{" "}
+                      {new Date(plan.created_at).toLocaleDateString()}
+                    </p>
+                  </div>
+                  <p className="price">${plan.price}</p>
+                </div>
+              ))
+            ) : (
+              <p className="font-bold">No plans purchased</p>
+            )}
+          </div>
+        </>
+      )}
     </>
   );
 };
