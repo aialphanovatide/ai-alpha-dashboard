2c5ece69971c733bbdcdf491e3e3da5c7589deaf
Camila <c.sosa@novatidelabs.com>
2024-09-23 19:22:23 -0300

    Modularize story card component

--- src/views/topStories/index.js
+++ src/views/topStories/index.js
@@ -1,109 +1,12 @@
 import React, { useEffect, useState } from "react";
 import config from "src/config";
 import "../analysis/analysis.css";
-import Swal from "sweetalert2";
-import CIcon from "@coreui/icons-react";
-import { cilTrash } from "@coreui/icons";
 import "./index.css";
-import { format } from "date-fns";
 import Pagination from "../pagination/Pagination";
 import SpinnerComponent from "src/components/Spinner";
 import NoData from "src/components/NoData";
 import Title from "src/components/commons/Title";
-import { AccessTime } from "@mui/icons-material";
-
-const Modal = ({ item, base64Image, onClose }) => {
-  const formattedSummary = item.analysis
-    ? item.analysis.replace(/\n/g, "<br>")
-    : item.content.replace(/\n/g, "<br>");
-
-  return (
-    <div className="modalOverlay" onClick={onClose}>
-      <div className="modalContent" onClick={(e) => e.stopPropagation()}>
-        {base64Image && (
-          <img
-            className="modalImage"
-            src={`https://appnewsposters.s3.us-east-2.amazonaws.com/${base64Image}`}
-            alt="top story"
-          />
-        )}
-        <span
-          className="modalText"
-          dangerouslySetInnerHTML={{ __html: formattedSummary }}
-        />
-        <button className="closeButton" onClick={onClose}>
-          Close
-        </button>
-      </div>
-    </div>
-  );
-};
-
-const Item = ({ item, base64Image, onDelete }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleClick = (e) => {
-    e.stopPropagation();
-    onDelete(item.id); // Usamos article_id aquí
-  };
-
-  const handleItemClick = (e) => {
-    if (
-      e.target.classList.contains("deleteBtn") ||
-      e.target.parentElement.classList.contains("deleteBtn")
-    ) {
-      return;
-    }
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
-
-  const formattedSummary = item.analysis
-    ? item.analysis.replace(/\n/g, "<br>")
-    : item.content.replace(/\n/g, "<br>");
-  const formattedDate = format(new Date(item.date), "MMMM dd, yyyy");
-  return (
-    <>
-      <div className="card" onClick={handleItemClick}>
-        {base64Image && (
-          <img
-            className="card-img-top"
-            src={`https://appnewsposters.s3.us-east-2.amazonaws.com/${base64Image}`}
-            alt="Analysis"
-          />
-        )}
-        <div className="card-body">
-          <h6 className="card-title"><strong>{item.title}</strong></h6><br></br>
-          <p
-            className="card-text"
-            dangerouslySetInnerHTML={{ __html: formattedSummary }}
-          />
-          <div className="card-date">
-            <p><AccessTime/>{" "}{formattedDate}</p>
-            {onDelete && (
-              <CIcon
-                size="lg"
-                icon={cilTrash}
-                className="deleteBtn"
-                onClick={handleClick}
-              />
-            )}
-          </div>
-        </div>
-      </div>
-      {isModalOpen && (
-        <Modal
-          item={item}
-          base64Image={base64Image}
-          onClose={handleCloseModal}
-        />
-      )}
-    </>
-  );
-};
+import StoryCard from "../StoryCard";
 
 const TopStories = () => {
   const [topStories, setTopStories] = useState([]);
@@ -120,7 +23,6 @@ const TopStories = () => {
 
       if (response1.ok) {
         const topStories = data1.data.filter((article) => article.is_top_story);
-        console.log("llegan estas: ", topStories);
         setTopStories(topStories);
       } else {
         console.error("Error fetching:", data1);
@@ -129,47 +31,6 @@ const TopStories = () => {
       console.error("Error fetching:", error);
     } finally {
       setLoading(false);
-    }
-  };
-
-  const handleDelete = async (article_id) => {
-    try {
-      const response = await fetch(
-        `${config.BOTS_V2_API}/api/update/top-story/${article_id}`,
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            "ngrok-skip-browser-warning": "true",
-          },
-        },
-      );
-
-      const data = await response.json();
-
-      if (response.ok) {
-        Swal.fire({
-          icon: "success",
-          title: data.message,
-          showConfirmButton: false,
-          timer: 1500,
-        });
-        getTopStories();
-      } else {
-        console.error("Error updating:", data);
-        Swal.fire({
-          icon: "error",
-          title: data.error,
-          showConfirmButton: false,
-        });
-      }
-    } catch (error) {
-      console.error("Error updating:", error);
-      Swal.fire({
-        icon: "error",
-        title: error,
-        showConfirmButton: false,
-      });
     }
   };
 
@@ -187,33 +48,33 @@ const TopStories = () => {
   return (
     <div className="alltopStoriesmain">
       <Title className="alltopStoriesTitle">Top Stories</Title>
-      {loading ? (
-        <SpinnerComponent />
-      ) : (
-        <>
-          {currentArticles && currentArticles.length > 0 ? (
-            <>
-              <div className="card-deck">
-                {currentArticles.map((item) => (
-                  <Item
-                    key={item.article_id}
-                    item={item}
-                    base64Image={item.image}
-                    onDelete={handleDelete}
-                  />
-                ))}
-              </div>
-              <Pagination
-                totalArticles={topStories.length}
-                articlesPerPage={articlesPerPage}
-                currentPage={currentPage}
-                setCurrentPage={setCurrentPage}
-              />
-            </>
-          ) : (
-            <NoData message={"No Top Stories yet"} />
-          )}
-        </>
+      <div
+        className="searchResults"
+        style={{
+          display: loading || currentArticles.length < 1 ? "flex" : "grid",
+        }}
+      >
+        {loading ? (
+          <SpinnerComponent style={{ margin: "auto" }} />
+        ) : (
+          <>
+            {currentArticles.length < 1 ? (
+              <NoData message={"No Top Stories yet"} />
+            ) : (
+              currentArticles.map((article, index) => (
+                <StoryCard key={index} article={article} getTopStories={getTopStories}/>
+              ))
+            )}
+          </>
+        )}
+      </div>
+      {!loading && (
+        <Pagination
+          totalArticles={topStories.length}
+          articlesPerPage={articlesPerPage}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
       )}
     </div>
   );
